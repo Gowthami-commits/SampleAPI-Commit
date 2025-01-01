c6ccc6e1ccba741ba65a6f3feedf762944d77e31
Gowthami Guddanti <gowthami.guddanti@pal.tech>
2025-01-01 23:05:13 +0530

    Initial commit

---
+++ DataBaseDbContext/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleAPI.Model;
+
+namespace SampleAPI.DataBaseDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options){ }
+
+        public ApplicationDbContext() { }
+
+        public virtual DbSet<Cities> Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cities>(entity =>
+            {
+                entity.ToTable("Cities", "dbo"); // Replace 'YourSchemaName' with the actual schema
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Model/Cities.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleAPI.Model
+{
+    public  class Cities
+    {
+        [Key]
+        public int CityId { get; set; }
+        public string? CityName { get; set; }
+        public int? EmployeeId { get; set; }
+        public string? EmployeeName { get; set; } = string.Empty;
+
+    }
+}
