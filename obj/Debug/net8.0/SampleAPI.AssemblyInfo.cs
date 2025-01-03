3e5017ca062cced16f84d88257461d50c46bb5b8
gowthami.guddanti <gowthami.guddanti@PTDELL0318>
2025-01-03 16:28:14 +0530

    Middleware Changes

--- obj/Debug/net8.0/SampleAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/SampleAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b0a2c330fe3f8bfdaf044c9eec55678457b7bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
